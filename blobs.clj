afd59776eb64a44213db583fbb0b0a30a9e6783e
Ali Honarvar <honarvar@gmail.com>
2009-03-17 22:09:00 -0400

    the jabber hook does not work yet. update accordingly.

--- blobs.clj
+++ blobs.clj
@@ -3,5 +3,5 @@
   [:p "Hello, World"]]
 
  ["Jabber" "Tuesday, March 17, 2009" 
-  [:p "Github can send you a message over jabber."]]
+  [:p "Github can " [:b "not"] " send you a message over jabber."]]
 ]
